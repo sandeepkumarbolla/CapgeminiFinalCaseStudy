de042abdb9b16cc32a77de1f51794278ae8e969b
sandeepkumarbolla <sandeepchowdarybolla159@gmail.com>
2021-05-02 07:55:21 +0530

    Add files via upload

---
+++ ApiGateWay/src/main/java/casestudy/zuul/ApiGateWay/ApiGateWayApplication.java
@@ -0,0 +1,16 @@
+package casestudy.zuul.ApiGateWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateWayApplication.class, args);
+	}
+}
---
+++ ApiGateWay/src/test/java/casestudy/zuul/ApiGateWay/ApiGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package casestudy.zuul.ApiGateWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
